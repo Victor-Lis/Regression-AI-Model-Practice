962c8d953df35acce54c1e7664ff0655440a6d56
Victor-Lis <victorlisbronzo1@gmail.com>
2024-01-03 12:24:13 -0300

    v1.2
    Atualização nas tabelas de dados.
    Atualização no script.py, agora é possível escolher modelo de data e a opção de cancelar o loop de perguntas.

--- script.py
+++ script.py
@@ -10,53 +10,83 @@ import numpy as np
 
 # Load Data 
 
-df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/delaney_solubility_with_descriptors.csv')
+dataType = ""
+df = ""
+while dataType == "":
+    dataType = input(f"Escolha uma opção: \n 1- Data1 \n 2- Data2 \nR:")
+# Data 1;
+if dataType == "1":
+    df = pd.read_csv('https://raw.githubusercontent.com/Victor-Lis/Regression-AI-Model-Practice/master/data.csv')
+
+# Data 2;
+if dataType == "2":
+    df = pd.read_csv('https://raw.githubusercontent.com/Victor-Lis/Regression-AI-Model-Practice/master/data2.csv')
+
+# Data Preparation 
+
+y = df["y"]
+
+x = df.drop("y", axis=1)
 
 
-# # Data Preparation 
+# Data Splitting
 
-# y = df["logS"]
-
-# x = df.drop("logS", axis=1)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=100)
 
 
-# # Data Splitting
+# Model Building 
 
-# x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=100)
+# Linear Regression
 
+## Training the model
 
-# # Model Building 
-
-
-# ## Linear Regression
-
-# ### Training the model
-
-# lr = LinearRegression()
-# lr.fit(x_train, y_train)
+lr = LinearRegression()
+lr.fit(x_train, y_train)
 
 
-# ### Applying the model to make a prediction
+### Applying the model to make a prediction
 
-# y_lr_train_pred = lr.predict(x_train)
-# y_lr_test_pred = lr.predict(x_test)
+y_lr_train_pred = lr.predict(x_train)
+y_lr_test_pred = lr.predict(x_test)
 
 
-# ### Evaluate Model Performace
+### Evaluate Model Performace
 
-# lr_train_mse = mean_squared_error(y_train, y_lr_train_pred)
-# lr_train_r2 = r2_score(y_train, y_lr_train_pred)
+lr_train_mse = mean_squared_error(y_train, y_lr_train_pred)
+lr_train_r2 = r2_score(y_train, y_lr_train_pred)
 
-# lr_test_mse = mean_squared_error(y_test, y_lr_test_pred)
-# lr_test_r2 = r2_score(y_test, y_lr_test_pred)
+lr_test_mse = mean_squared_error(y_test, y_lr_test_pred)
+lr_test_r2 = r2_score(y_test, y_lr_test_pred)
 
-# print("LR MSE (Train): ", lr_train_mse)
-# print("LR R2 (Train): ", lr_train_r2)
-# print("LR MSE (Test): ", lr_test_mse)
-# print("LR R2 (Test): ", lr_test_r2)
-# print()
-
-# lr_results = pd.DataFrame(["Linear Regression", lr_train_mse, lr_train_r2, lr_test_mse, lr_test_r2]).transpose()
-# lr_results.columns = ['Method', 'Training MSE', 'Training R2', 'Test MSE', 'Test R2']
+lr_results = pd.DataFrame(["Linear Regression", lr_train_mse, lr_train_r2, lr_test_mse, lr_test_r2]).transpose()
+lr_results.columns = ['Method', 'Training MSE', 'Training R2', 'Test MSE', 'Test R2']
+# print("Result Analysis")
 # print(lr_results)
 # print()
+
+# Using
+
+## Predict Function 
+def predict():
+
+    ## Getting number from user
+    num = ""
+    while num == "":
+        num = input("Escreva um número: ")
+
+    ## Convert the input number to a list with a single element
+    new_data = pd.DataFrame([[float(num)]], columns=['x'])  # Assign the feature name 'x'
+
+    ## Make a prediction using the trained model
+    prediction = lr.predict(new_data)
+
+    ## Print the prediction result
+    print("Valor:", prediction[0])
+    print()
+
+    ## Restart
+    restart = input("Recomeçar? y/n")
+    if restart == "y":
+        predict()
+
+predict()
